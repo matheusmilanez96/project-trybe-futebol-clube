a7e300f06a5bd68615623185668f4c8703efd00e
Matheus Milanez <mattmila96@gmail.com>
2023-07-18 20:20:50 -0300

    router fix

--- app/backend/src/routes/auth.routes.ts
+++ app/backend/src/routes/auth.routes.ts
@@ -1,10 +1,15 @@
 import { Request, Router, Response } from 'express';
 import AuthController from '../controllers/AuthController';
+import Validations from '../middlewares/Validations';
 
 const authController = new AuthController();
 
 const router = Router();
 
-router.post('/', (req: Request, res: Response) => authController.getUserByEmail(req, res));
+router.post(
+  '/',
+  Validations.validateLogin,
+  (req: Request, res: Response) => authController.getUserByEmail(req, res),
+);
 
 export default router;
--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import teamsRouter from './team.routes';
 import authRouter from './auth.routes';
-import Validations from '../middlewares/Validations';
 
 const router = Router();
 
 router.use('/teams', teamsRouter);
-router.use('/login', Validations.validateLogin, authRouter);
+router.use('/login', authRouter);
 
 export default router;
